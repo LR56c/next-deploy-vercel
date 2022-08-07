92c3b1623f1f01f777c80b106d33182dac410477
Mauricio Herrera Ortega <ronalli56@gmail.com>
2022-08-07 10:42:03 -0400

    pre-deploy

---
+++ pages/about.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { MainLayout } from "../components/layouts/MainLayout";
+import { DarkLayout } from "../components/layouts/DarkLayout";
+import { ReactElement } from "react";
+import { NextPageWithLayout } from "./_app";
+
+const AboutPage: NextPageWithLayout = () =>
+{
+  return (
+    <>
+      <h1>About Page</h1>
+      <h1 className="title">
+        Ir a <Link href="/">Home</Link>
+      </h1>
+      <p className="description">
+        Get started by editing{ ' ' }
+        <code className="code">pages/index.tsx</code>
+      </p>
+    </>
+  )
+}
+
+AboutPage.getLayout = ( page: ReactElement ) =>
+{
+  return (
+    <MainLayout>
+      <DarkLayout>
+        { page }
+      </DarkLayout>
+    </MainLayout>
+  )
+}
+
+export default AboutPage
